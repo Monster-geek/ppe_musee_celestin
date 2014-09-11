d08c17ca52a52ce9e971e8ba9ac81d32d210da91
ITG_MonsterGeek <alexlegrem@gmail.com>
2014-09-11 14:08:03 +0200

    Upload de ressources
    Upload des fichier de base pour servir de ressources.

---
+++ Classe technique/Main.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+// Lien vers la classe TECHNIQUE
+using TECHNIQUE;
+
+namespace TEST
+{
+    // Classe de TEST
+    class MainClass
+	{
+		public static void Main(string[] args)
+		{
+            // Instanciation d'un objet de la classe technique 'Donnees'
+            Donnees mesDonnees = new Donnees("DSNmabd");
+
+            try
+            {
+                //-----------------------------------------
+                // Connexion, puis récupération du résultat
+                //-----------------------------------------
+                mesDonnees.Connexion();
+
+                //----------------
+                // Requêtes SELECT
+                //----------------
+
+                string texteSQL = "SELECT CONCAT(fournisseur.numero, ', ', fournisseur.nom, ', ', prenom) AS FOURNISSEUR,";
+                texteSQL += "CONCAT(articles.nom, ', ', prix) AS ARTICLE,";
+                texteSQL += "CONCAT('Actuel : ', stockactuel, ' Min: ', stockminimum) AS STOCK";
+                texteSQL += " FROM fournisseur, articles";
+                texteSQL += " WHERE fournisseur.numero = articles.numero";
+                texteSQL += " ORDER BY fournisseur.nom, articles.nom";
+                Console.WriteLine(mesDonnees.ExecuterSQL(texteSQL, true, "Liste des produits par fournisseurs"));
+
+                Console.ReadKey();
+                Console.Clear();
+
+                texteSQL = "SELECT COUNT(*) AS NOMBRE FROM articles";
+                Console.WriteLine(mesDonnees.ExecuterSQL(texteSQL, true, "Nombre d'articles"));
+
+                Console.ReadKey();
+                Console.Clear();
+
+                //-------
+                // UPDATE
+                //-------
+                texteSQL = "UPDATE articles SET stockminimum=15 WHERE code='b500'";
+                Console.WriteLine(mesDonnees.ExecuterSQL(texteSQL, false, "MAJ du stock de l'article 'b500' avec 15 unités"));
+
+                Console.ReadKey();
+                Console.Clear();
+
+                //-----------------------
+                // Infos sur la connexion
+                //-----------------------
+                Console.WriteLine(mesDonnees.InfosSurConnexion());
+            }
+            catch (Exception ex)
+                { Console.WriteLine(ex.Message); } 
+		    finally
+                { mesDonnees.Fermeture(); } // Dans tous les cas on ferme tout !
+	
+
+            Console.ReadKey();
+		}
+	}
+}
---
+++ Classes métier et de test/Musee/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using METIER;
+
+namespace TEST
+{
+    //---------------
+    // Classe de TEST
+    //---------------
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Des artistes
+            Artiste monet = new Artiste("Monet", "Francaise");
+            Artiste manet = new Artiste("Manet", "Francaise");
+            Artiste vangogh = new Artiste("Van Gogh", "Neelandaise");
+
+            // Des oeuvres
+            Oeuvre o1 = new Oeuvre("Le Déjeuner sur l'herbe", 500000);
+            Oeuvre o2 = new Oeuvre("Au bord de l'eau", 350000);
+            Oeuvre o3 = new Oeuvre("La Partie de croquet", 250000);
+            Oeuvre o4 = new Oeuvre("Tournesols dans un vase", 1000000);
+            Oeuvre o5 = new Oeuvre("Champ de ble avec cypres", 100000);
+            Oeuvre o6 = new Oeuvre("Les Paveurs", 275000);
+
+            // Des salles
+            Salle francaise = new Salle("Francaise", 10000000);
+            Salle neerlandaise = new Salle("Neerlandaise", 5000000);
+
+            // Un musée
+            Musee monMusee = new Musee("Musee des Celestins - VICHY");
+
+            // Ajouter des Artistes
+            monMusee.CreerArtiste(monet);
+            monMusee.CreerArtiste(manet);
+            monMusee.CreerArtiste(vangogh);
+
+            // Ajouter des Salles
+            monMusee.CreerSalle(francaise);
+            monMusee.CreerSalle(neerlandaise);
+
+            // Ajouter des Oeuvres
+            monMusee.CreerOeuvre(o1, monet, monMusee.GetSalle(0));
+            monMusee.CreerOeuvre(o2, monet, monMusee.GetSalle(0));
+            monMusee.CreerOeuvre(o3, manet, monMusee.GetSalle(0));
+            monMusee.CreerOeuvre(o4, vangogh, monMusee.GetSalle(1));
+            monMusee.CreerOeuvre(o5, vangogh, monMusee.GetSalle(1));
+            monMusee.CreerOeuvre(o6, vangogh, monMusee.GetSalle(1));
+
+            // Afficher le musée
+            monMusee.AfficherMusee();
+
+            Console.ReadKey();
+        }
+    }
+}
